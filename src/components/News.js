2e85cfe7db1299cce38da7dab3734f0e9addcb80
Pranivvv <21104031.praniv.warungshe@gmail.com>
2024-11-09 11:38:48 +0530

    completed loading icon functionality for my app
    added loading icon nd made it appear only when app is fetching news from server nd for that time removed the feed of news from user screen

--- src/components/News.js
+++ src/components/News.js
@@ -16,33 +16,42 @@ export class News extends Component {
   
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=56593e3a139c4e5f8b5c1a1e474239e6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
-    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    this.setState({
+      articles: parsedData.articles, 
+      totalResults: parsedData.totalResults,
+      loading:false
+    })
   }
 
   prevPage= async ()=>{
     console.log(this.state.page-1)
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=56593e3a139c4e5f8b5c1a1e474239e6&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
-      page:this.state.page-1
+      page:this.state.page-1,
+      loading:false
     })
   }
 
   nextPage= async ()=>{
     console.log(this.state.page+1)
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=56593e3a139c4e5f8b5c1a1e474239e6&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
-      page:this.state.page+1
+      page:this.state.page+1,
+      loading:false
     })
   }
 
@@ -50,8 +59,10 @@ export class News extends Component {
     return (
       <div className='container d-flex align-items-center flex-column  mb-3' data-bs-theme={this.props.mode}>
         <h1 className='my-3 ' >Top News - From all around the world</h1>
-        <Spinner />
-        <div className='d-flex flex-wrap justify-content-center' >
+        {this.state.loading && <Spinner />}
+        {
+          (!this.state.loading) &&
+          <div className='d-flex flex-wrap justify-content-center' >
           {this.state.articles.map((article)=>{
             const { urlToImage: imageUrl, title, description, url: newsUrl } = article;
             if(imageUrl && title && description && newsUrl){
@@ -61,6 +72,7 @@ export class News extends Component {
             }
           })}
         </div>
+        }
         <div className="container d-flex flex-wrap justify-content-around">
           <button disabled={this.state.page<=1} type="button" className={`btn btn-outline-${this.props.mode==='light'?'dark':'light'}`} onClick={this.prevPage}>&larr; Secondary</button>
           <button disabled={(this.state.page + 1)>Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className={`btn btn-outline-${this.props.mode==='light'?'dark':'light'}`} onClick={this.nextPage}>Next &rarr;</button>
