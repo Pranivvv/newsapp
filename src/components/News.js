280c8b13309b38f92282f6b424d4542dc409dddd
Pranivvv <21104031.praniv.warungshe@gmail.com>
2024-11-23 00:38:26 +0530

    creaated update news function to reuse it to updare news for next nd previous button

--- src/components/News.js
+++ src/components/News.js
@@ -33,8 +33,7 @@ export class News extends Component {
     }
   }
 
-  
-  async componentDidMount() {
+  async updateNews(){
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true})
     let data = await fetch(url);
@@ -46,33 +45,21 @@ export class News extends Component {
       loading:false
     })
   }
+  
+  async componentDidMount() {
+    this.updateNews()
+  }
 
   prevPage= async ()=>{
-    console.log(this.state.page-1)
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true})
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles,
-      page:this.state.page-1,
-      loading:false
-    })
+    // console.log(this.state.page-1)
+    this.setState({page:this.state.page-1})
+    this.updateNews()
   }
 
   nextPage= async ()=>{
-    console.log(this.state.page+1)
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true})
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles,
-      page:this.state.page+1,
-      loading:false
-    })
+    // console.log(this.state.page+1)
+    this.setState({page:this.state.page+1})
+    this.updateNews()
   }
 
   render() {
@@ -94,7 +81,7 @@ export class News extends Component {
         </div>
         }
         <div className="container d-flex flex-wrap justify-content-around">
-          <button disabled={this.state.page<=1} type="button" className={`btn btn-outline-${this.props.mode==='light'?'dark':'light'}`} onClick={this.prevPage}>&larr; Secondary</button>
+          <button disabled={this.state.page<=1} type="button" className={`btn btn-outline-${this.props.mode==='light'?'dark':'light'}`} onClick={this.prevPage}>&larr; Previous </button>
           <button disabled={(this.state.page + 1)>Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className={`btn btn-outline-${this.props.mode==='light'?'dark':'light'}`} onClick={this.nextPage}>Next &rarr;</button>
         </div>
       </div>
